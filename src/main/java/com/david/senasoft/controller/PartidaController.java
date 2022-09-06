46fe8214fc5658059a172271fff68d6dbb737888
DavidValbuena1 <104107409+DavidValbuena1@users.noreply.github.com>
2022-09-06 14:07:11 -0500

    Add files via upload

--- src/main/java/com/david/senasoft/controller/PartidaController.java
+++ src/main/java/com/david/senasoft/controller/PartidaController.java
@@ -3,16 +3,22 @@ package com.david.senasoft.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.david.senasoft.model.Partida;
 import com.david.senasoft.service.PartidaService;
 
+@CrossOrigin(origins="*")
+@RestController
+@RequestMapping({"/Senasoft/Partida"})
 public class PartidaController {
 	@Autowired(required=true)
 	private PartidaService service;
