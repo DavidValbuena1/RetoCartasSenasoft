5e1e17a56871125a8f6b9a20b0baf16d2f7a58a2
Marlong Mendoza <marlongyes@gmail.com>
2022-09-07 16:47:15 -0500

    version 6 frontend

--- FrontEnd/src/app/inicio/inicio.component.ts
+++ FrontEnd/src/app/inicio/inicio.component.ts
@@ -24,50 +24,36 @@ export class InicioComponent implements OnInit {
     if(this.codigoPartida != ""){
       this.btnDisabledUnirme = false
       console.log(this.listaUsuarios);
-
     }
   }
   /*  */
-
-  crearUsuario(tipoPartida:string){
+  crearUsuario(tipoUs:string){
     let idPartidaCrearUsuario:any;
     let partida = JSON.parse(localStorage.getItem("dataPartida") || "[]")
-/* console.log(partida); */
-
-
 this.partidaService.getPartidas().subscribe((x:any)=> {
   for(let p of x){
 
     if(p.codigo == partida.codigo){
       idPartidaCrearUsuario = p.idpartida;
-      console.log(idPartidaCrearUsuario);
-
-
       let data = {
         idusuario:0,
       nombre: this.nombreJugador,
       partida: {
           idpartida:p.idpartida,
       },
-      tipoUsuario: "Administrador",
-      resultado: "ganador"
+      tipoUsuario: tipoUs,
+      resultado: "En espera"
       }
       try {
-        console.log(data);
         this.usuarioService.postUsuario(data)
-        console.log("usuario creadooooo");
       } catch (error) {
         console.log(error);
-
       }
       localStorage.setItem("dataUsuario", JSON.stringify(data))
       this.insercionUsuarioPartida()
     }
   }
 })
-    /* console.log(idPartidaCrearUsuario); */
-
-
   }
   /* CREACION CODIGO HEXADECIMAL ALEATORIO */
   generarLetra(){
@@ -96,8 +82,6 @@ this.partidaService.getPartidas().subscribe((x:any)=> {
         if(p.codigo == partida.codigo){
          this.idPartida = p.idpartida;
          console.log(this.idPartida);
-
-
 /* --------- */
 this.usuarioService.getUsuarios().subscribe((x:any)=> {
   for(let u of x){
@@ -118,20 +102,14 @@ this.usuarioService.getUsuarios().subscribe((x:any)=> {
         this.jugadorPartidaService.getJugadorPartidas(this.idPartida).subscribe((x:any)=>x)
         this.router.navigate(['partida']);
       }catch(err){
-
+        console.log(err);
       }
-
     }else{
       console.log("vuelve a enviarlo")
     }
   }
-
 })
 /* --------- */
-
-
-
-
         }
       }
     })
@@ -154,10 +132,6 @@ this.usuarioService.getUsuarios().subscribe((x:any)=> {
       }
 
     }) */
-
-
-
-
    }
   /* CREACION DE UNA PARTIDA */
   crearPartida(){
@@ -186,6 +160,114 @@ this.usuarioService.getUsuarios().subscribe((x:any)=> {
 /* this.partidaService.getPartidas().subscribe((x:any)=>x); */
   /* console.log(this.colorHEX()) */
 
+  }
+  unirmePartida(){
+/*  */
+let partida = JSON.parse(localStorage.getItem("dataPartida") || "[]")
+this.partidaService.getPartidas().subscribe((x:any)=> {
+  for(let p of x){
+    console.log(x);
+
+    if(p.codigo == this.codigoPartida){
+     /*  idPartidaCrearUsuario = p.idpartida; */
+      console.log(p.codigo);
+      /* ID PARTIDA */
+      console.log(p.idpartida);
+/* ID JUGADOR */
+let data = {
+  idusuario:0,
+nombre: this.nombreJugador,
+partida: {
+    idpartida:p.idpartida,
+},
+tipoUsuario: "jugador",
+resultado: "En espera"
+}
+try {
+  this.usuarioService.postUsuario(data)
+  console.log("USUSARIO CREADOOO");
+  this.usuarioService.getUsuarios().subscribe((x:any)=> {
+    for(let u of x){
+      console.log(this.nombreJugador);
+
+      if(u.nombre == this.nombreJugador){
+      console.log(this.nombreJugador);
+
+       /*  console.log(u.idusuario);
+        console.log(u.idpartida); */
+
+
+        this.idJugador = u.idusuario;
+
+        try {
+
+          new Promise((resolve,reject)=>{
+            let datos = {
+              partida:{
+                idpartida:p.idpartida
+            },
+            jugador:{
+                idusuario:u.idusuario
+            }
+            }
+            console.log(datos);
+
+            resolve(this.jugadorPartidaService.postJugadorPartida(datos))
+            reject("error de envio a tabla")
+          })
+.then((x)=>console.log("PUDIMOS CARGARLO"))
+.catch((x)=>console.log("NO PUDIMOS "))
+          console.log("se insertarion datos a jugadorpartida");
+
+        } catch (error) {
+            console.log(error);
+        }
+      }
+    }
+  })
+} catch (error) {
+  console.log(error);
+}
+
+
+
+    /*   let data = {
+        idusuario:0,
+      nombre: this.nombreJugador,
+      partida: {
+          idpartida:p.idpartida,
+      },
+      tipoUsuario: "Administrador",
+      resultado: "ganador"
+      } */
+      try {
+        /* this.usuarioService.postUsuario(data) */
+      } catch (error) {
+        console.log(error);
+      }
+     /*  localStorage.setItem("dataUsuario", JSON.stringify(data)) */
+     /*  this.insercionUsuarioPartida() */
+    }
+  }
+})
+/*  */
+    try {
+
+      /* this.crearUsuario("");------ */
+    } catch (error) {
+      console.log(error);
+
+    }
+  }
+
+  insercionUsuarioPartidaExistente(data:any){
+    try {
+      this.jugadorPartidaService.postJugadorPartida(data)
+      console.log("se insertarion datos a jugadorpartida");
+
+    } catch (error) {
+        console.log(error);
+    }
   }
   /*  */
   listaUsuarios:any;
